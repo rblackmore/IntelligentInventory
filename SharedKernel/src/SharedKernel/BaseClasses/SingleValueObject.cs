671cb137e41d707b1890cf57bee237912b9eb8ed
Ryan Blackmore <3052414+rblackmore@users.noreply.github.com>
2022-02-11 17:08:18 +1100

    SingleValueObject BaseClass Can be used for simple ID ValueObjects with only a single primitive value to encapsulate.

---
+++ SharedKernel/src/SharedKernel/BaseClasses/SingleValueObject.cs
@@ -0,0 +1,44 @@
+﻿namespace IntelligentInventory.SharedKernel.BaseClasses;
+
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+public abstract class SingleValueObject<TValue, TThis> : ValueObject
+  where TThis : SingleValueObject<TValue, TThis>, new()
+{
+  private static readonly Func<TThis> Factory;
+
+  static SingleValueObject()
+  {
+    ConstructorInfo ctor = typeof(TThis)
+        .GetTypeInfo()
+        .DeclaredConstructors
+        .First();
+
+    var argsExp = new Expression[0];
+    NewExpression newExp = Expression.New(ctor, argsExp);
+    LambdaExpression lambda = Expression.Lambda(typeof(Func<TThis>), newExp);
+
+    Factory = (Func<TThis>)lambda.Compile();
+  }
+
+  public TValue Value { get; protected set; }
+
+  protected override IEnumerable<object> GetEqualityComponents()
+  {
+    yield return this.Value;
+  }
+
+  protected abstract void Validate();
+
+  public static TThis From(TValue item)
+  {
+    TThis x = Factory();
+    x.Value = item;
+    x.Validate();
+
+    return x;
+  }
+}
---
+++ SharedKernel/tests/UnitTests/ValueObjectTests/SingleValueObject_Tests.cs
@@ -0,0 +1,69 @@
+﻿namespace UnitTests.ValueObjectTests;
+
+using System;
+
+using AutoFixture.Xunit2;
+
+using FluentAssertions;
+
+using IntelligentInventory.SharedKernel.BaseClasses;
+
+using Xunit;
+
+internal class MyEntityId : SingleValueObject<Guid, MyEntityId>
+{
+  protected override void Validate()
+  {
+    if (this.Value == Guid.Empty)
+      throw new ArgumentException("Empty id", nameof(this.Value));
+
+    if (this.Value == default(Guid))
+      throw new ArgumentException("Default id", nameof(this.Value));
+  }
+
+  public static MyEntityId Create()
+  {
+    return MyEntityId.From(Guid.NewGuid());
+  }
+}
+
+public class SingleValueObject_Tests
+{
+  [Theory]
+  [AutoData]
+  public void Create_Success(Guid guid)
+  {
+    var id = MyEntityId.From(guid);
+
+    id.Value.Should().Be(guid);
+  }
+
+  [Fact]
+  public void Create_SuccessFromStatic()
+  {
+    var id = MyEntityId.Create();
+
+    id.Value.Should().NotBe(Guid.Empty);
+    id.Value.Should().NotBe(default(Guid));
+  }
+
+  [Fact]
+  public void Throws_ArgumentException_WithEmptyGuid()
+  {
+    var id = Guid.Empty;
+
+    var create = () => MyEntityId.From(id);
+
+    create.Should().Throw<ArgumentException>();
+  }
+
+  [Fact]
+  public void Throws_ArgumentException_WithDefaultGuid()
+  {
+    var id = default(Guid);
+
+    var create = () => MyEntityId.From(id);
+
+    create.Should().Throw<ArgumentException>();
+  }
+}
